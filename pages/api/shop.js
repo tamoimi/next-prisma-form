e12bf2de1c15478f47367ced2d3274a5e3d51c50
tami <xorud7979@gmail.com>
2022-09-19 14:22:21 +0900

    feat: 숫자 에러 수정
    -parseInt()를 사용해 숫자로 입력되는 값을 string에서 number로 바꿔줄 수 있다.

--- pages/api/shop.js
+++ pages/api/shop.js
@@ -1,4 +1,4 @@
-import client from "../"
+import client from "../libs/prismaClient";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
@@ -9,16 +9,19 @@ export default async function handler(req, res) {
         data: { shopName, shopMenu, shopPrice },
       },
     } = req;
+
     console.log("백엔드 로그: ", shopName, shopMenu, shopPrice);
 
-    const result = await client.user.create({
-      date: {
+    const result = await client.shop.create({
+      data: {
         shopName,
         shopMenu,
-        shopPrice,
+        shopPrice: parseInt(shopPrice),
       },
-    })
+    });
+
+    res.status(200).json(result);
   }
-  res.status(200).json({});
-  //여기에서 .json의 역할은 <- 이곳으로 값을 넣겠다 
 }
+
+//여기에서 .json의 역할은 <- 이곳으로 값을 넣겠다
